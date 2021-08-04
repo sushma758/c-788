4654b4a7a822db74272ab6b18eee0c67ff9eba20
sushma758 <78887242+sushma758@users.noreply.github.com>
2021-08-04 09:24:32 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+
+var images = ["https://i.postimg.cc/MKdhy06Z/family.jpg",
+"https://i.postimg.cc/wjMnFtMX/father.jpg" , 
+"https://i.postimg.cc/5ymDKL83/bro.jpg", 
+"https://i.postimg.cc/JnL6wtrd/sister.jpg", 
+"https://i.postimg.cc/bw5W5zSK/mother.jpg"];
+var names = ["Family Book", 
+"Ravi dyapa", 
+"Aarav dyapa", 
+"Anwita dyapa", 
+"sushma dyapa"];
+var i = 0;
+function update()
+{
+    i++;
+    var numbers_of_family_member_in_array = 4;
+    if(i > numbers_of_family_member_in_array )
+    {
+        i = 0;
+    }
+    var updatedImage = images[i];
+    var updatedName  = names[i];
+    document.getElementById("family_member_image").src = updatedImage;
+    document.getElementById("family_member_name").innerHTML = updatedName;
+}
+
